616ea288b729a23beb486ad8d8ecf57577d117c9
Victor Rodrigues <46715082+Victorpoa@users.noreply.github.com>
2022-04-09 03:25:57 -0300

    atualização 09/04/2022

--- js/index.js
+++ js/index.js
@@ -1,32 +1,3 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
-document.addEventListener('deviceready', onDeviceReady, false);
-
-function onDeviceReady() {
-    // Cordova is now initialized. Have fun!
-
-    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
-    document.getElementById('deviceready').classList.add('ready');
-}
 function vermenu(){
     var menu = document.querySelector("#menu")
     var corpo_menu = document.querySelector("#corpo_menu")
@@ -36,3 +7,6 @@ function vermenu(){
         corpo_menu.style.display = "block"
     }
 }
+var musica = document.querySelector("#music")
+
+musica.volume = 0.2
